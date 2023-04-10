b7f1d80acf653f2451d996717a7259dbc9d14d52
pati <pati@ciklum.com>
2023-04-10 16:34:42 +0200

    terraform out

--- src/forecasting/src/ewmac/terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lambda_function_arn" {
-  value = aws_lambda_function.lambda.arn
-}
-
-output "sqs_queue_url" {
-  value = aws_sqs_queue.sqs.url
-}
--- src/shared/layers/dynamo_context/terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "layer_arn" {
-  description = "The ARN of the Lambda layer"
-  value       = aws_lambda_layer_version.dynamo_context.arn
-}
