b445013856b3107dc1f61f9c09a5335cc4752b8a
pati <pati@ciklum.com>
2023-05-01 21:53:18 +0200

    ewmac rule and stepfunction was added into main.tf

--- src/forecasting/rules/ewmac/ewmac_rule.py
+++ src/forecasting/rules/ewmac/ewmac_rule.py
@@ -3,7 +3,7 @@ import pandas as pd
 from typing import Dict, Any
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from shared.layers.helpers.python.estimators.vol import robust_vol_calc
+from estimators.vol import robust_vol_calc
 logger = Logger()
 
 def process_data(data: Dict[str, Any]) -> Dict[str, Any]:
