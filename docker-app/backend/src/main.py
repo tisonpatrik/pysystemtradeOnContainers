c8b8a27527017f1e6f954f73356b33863c43b412
PatrikTison <patriktison@gmail.com>
2023-07-07 20:14:45 +0200

    docker was updated, frontend was added

---
+++ docker-app/backend/src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Hello, beautiful"}
