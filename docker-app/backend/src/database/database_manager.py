96a80bbcd81240f139e9657ffd3cd282f9f54bb3
PatrikTison <patriktison@gmail.com>
2023-07-10 10:14:28 +0200

    db was refactored

--- docker-app/backend/src/database/database_manager.py
+++ docker-app/backend/src/database/database_manager.py
@@ -1,37 +1,12 @@
-import os
-import psycopg2
 from psycopg2 import sql
-from dotenv import load_dotenv
-from contextlib import contextmanager
-
-load_dotenv()
+from src.database.database_connection import DatabaseConnection
 
 class DatabaseManager:
-    def __init__(self):
-        self.DB_HOST = os.getenv('DB_HOST')
-        self.DB_NAME = os.getenv('DB_NAME')
-        self.DB_USER = os.getenv('DB_USER')
-        self.DB_PASSWORD = os.getenv('DB_PASSWORD')
-        self.DB_PORT = os.getenv('DB_PORT')
-
-    @contextmanager
-    def connection_cursor(self):
-        conn = psycopg2.connect(
-            dbname=self.DB_NAME,
-            user=self.DB_USER,
-            password=self.DB_PASSWORD,
-            host=self.DB_HOST,
-            port=self.DB_PORT
-        )
-        cursor = conn.cursor()
-        try:
-            yield cursor
-        finally:
-            cursor.close()
-            conn.close()
+    def __init__(self, db_connection:DatabaseConnection):
+        self.db_connection = db_connection
 
     def create(self, table, data):
-        with self.connection_cursor() as cursor:
+        with self.db_connection.connection_cursor() as cursor:
             insert = sql.SQL('INSERT INTO {} ({}) VALUES ({})').format(
                 sql.Identifier(table),
                 sql.SQL(', ').join(map(sql.Identifier, data.keys())),
@@ -41,7 +16,7 @@ class DatabaseManager:
             cursor.connection.commit()
 
     def read(self, table, condition=None):
-        with self.connection_cursor() as cursor:
+        with self.db_connection.connection_cursor() as cursor:
             if condition:
                 select = sql.SQL('SELECT * FROM {} WHERE {}').format(
                     sql.Identifier(table),
@@ -56,14 +31,14 @@ class DatabaseManager:
             return rows
 
     def update(self, table, data, condition):
-        with self.connection_cursor() as cursor:
+        with self.db_connection.connection_cursor() as cursor:
             set_clause = ', '.join([f'{column} = %({column})s' for column in data.keys()])
             update = sql.SQL(f'UPDATE {table} SET {set_clause} WHERE {condition}')
             cursor.execute(update, data)
             cursor.connection.commit()
 
     def delete(self, table, condition):
-        with self.connection_cursor() as cursor:
+        with self.db_connection.connection_cursor() as cursor:
             delete = sql.SQL(f'DELETE FROM {table} WHERE {condition}')
             cursor.execute(delete)
             cursor.connection.commit()
