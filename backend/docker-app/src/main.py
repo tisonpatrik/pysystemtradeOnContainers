fcf928125183cb3ec75f61cbe3ef218dabf2b4d5
PatrikTison <patriktison@gmail.com>
2023-07-24 11:13:56 +0200

    dockerfile works

--- backend/docker-app/src/main.py
+++ backend/docker-app/src/main.py
@@ -1,23 +1,7 @@
 from fastapi import FastAPI, status
-from api.router import router
-from core.config import settings
-from db.sessions import create_tables
 
-app = FastAPI(
-    title=settings.title,
-    version=settings.version,
-    description=settings.description,
-    root_path=settings.openapi_prefix,  # Updated this line
-    docs_url=settings.docs_url,
-    openapi_url=settings.openapi_url,
-)
-
-app.include_router(router, prefix=settings.api_prefix)
+app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"Say": "Hello!"}
-
-@app.get("/init_tables", status_code=status.HTTP_200_OK, name="init_tables")
-async def init_tables():
-    create_tables()
