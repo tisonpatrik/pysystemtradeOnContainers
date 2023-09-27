99cb5cdf9fc2321ead81a9d7a793c73f2a2ef1c4
PatrikTison <patriktison@gmail.com>
2023-09-27 21:36:58 +0200

    test table dropper

--- backend/postgres-seed/tests/conftest.py
+++ backend/postgres-seed/tests/conftest.py
@@ -1,7 +1,20 @@
 # fixture.py
 import pytest_asyncio
-from unittest.mock import Mock
 
 @pytest_asyncio.fixture
-def mock_connection():
-    return Mock()
+def mock_connection(mocker):
+    return mocker.Mock()
+
+# Define AsyncContextManagerMock as a pytest fixture
+@pytest_asyncio.fixture
+def async_context_manager_mock():
+    class AsyncContextManagerMock:
+        async def __aenter__(self):
+            return self
+
+        async def __aexit__(self, exc_type, exc_value, traceback):
+            pass
+
+        def __call__(self):
+            return self
+    return AsyncContextManagerMock()
--- backend/postgres-seed/tests/unit/test_table_creator.py
+++ backend/postgres-seed/tests/unit/test_table_creator.py
@@ -1,7 +1,5 @@
 import pytest
-from unittest.mock import patch, call
 from src.db.table_creator import TableCreator
-
 
 # Test __init__ method
 def test_init(mock_connection):
@@ -10,34 +8,34 @@ def test_init(mock_connection):
 
 # Test create_table with successful execution
 @pytest.mark.asyncio
-async def test_create_table_success(mock_connection):
+async def test_create_table_success(mock_connection, mocker):
     async def mock_execute(sql):
         pass
     
     mock_connection.execute = mock_execute
     sql_command = "CREATE TABLE test (id SERIAL PRIMARY KEY);"
 
-    with patch('src.db.table_creator.logger') as mock_logger:
-        table_creator = TableCreator(mock_connection)
-        await table_creator.create_table(sql_command)
+    mock_logger = mocker.patch('src.db.table_creator.logger')
+    table_creator = TableCreator(mock_connection)
+    await table_creator.create_table(sql_command)
 
     mock_logger.info.assert_has_calls([
-        call("Successfully executed the following SQL command: %s", sql_command),
-        call("Database connection closed.")
+        mocker.call("Successfully executed the following SQL command: %s", sql_command),
+        mocker.call("Database connection closed.")
     ])
 
 # Test create_table with failed execution
 @pytest.mark.asyncio
-async def test_create_table_fail(mock_connection):
+async def test_create_table_fail(mock_connection, mocker):
     async def mock_execute(sql):
         raise Exception("Some error")
 
     mock_connection.execute = mock_execute
     sql_command = "CREATE TABLE test (id SERIAL WRONG KEY);"
 
-    with patch('src.db.table_creator.logger') as mock_logger:
-        table_creator = TableCreator(mock_connection)
-        await table_creator.create_table(sql_command)
+    mock_logger = mocker.patch('src.db.table_creator.logger')
+    table_creator = TableCreator(mock_connection)
+    await table_creator.create_table(sql_command)
 
     # Get the last call's arguments
     args, kwargs = mock_logger.error.call_args
@@ -48,19 +46,17 @@ async def test_create_table_fail(mock_connection):
     # Check the exception's string representation
     assert str(args[1]) == "Some error"
 
-
-
 # Test create_table finally block
 @pytest.mark.asyncio
-async def test_create_table_finally(mock_connection):
+async def test_create_table_finally(mock_connection, mocker):
     async def mock_execute(sql):
         pass
 
     mock_connection.execute = mock_execute
     sql_command = "CREATE TABLE test (id SERIAL PRIMARY KEY);"
 
-    with patch('src.db.table_creator.logger') as mock_logger:
-        table_creator = TableCreator(mock_connection)
-        await table_creator.create_table(sql_command)
+    mock_logger = mocker.patch('src.db.table_creator.logger')
+    table_creator = TableCreator(mock_connection)
+    await table_creator.create_table(sql_command)
 
     mock_logger.info.assert_called_with("Database connection closed.")
