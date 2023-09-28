bd4be65fa29e5f9847f7b5325399c71bf47abaee
PatrikTison <patriktison@gmail.com>
2023-09-28 20:41:46 +0200

    small refactoring of preprocessing module

--- backend/postgres-seed/src/data_processing/data_preprocessor.py
+++ backend/postgres-seed/src/data_processing/data_preprocessor.py
@@ -42,27 +42,32 @@ def load_and_rename_columns(file_path, column_mapping):
         logger.error("Error processing data from %s: %s", file_path, error)
         raise ColumnRenamingError from error
 
+def process_single_csv_file(file, file_path):
+    try:
+        time_column_name = file.columns[0]
+        aggregated = aggregate_to_day_based_prices(file, time_column_name)
+        converted = convert_datetime_to_unixtime(aggregated, time_column_name)
+        data_frame = add_symbol_by_file_name(converted, file_path)
+        logger.info("Successfully loaded and added symbol for %s.", file_path)
+        return data_frame
+    except FileNotFoundError as exc:
+        logger.error("CSV file not found: %s", file_path)
+        raise CSVFileNotFoundError from exc
+    except Exception as error:
+        logger.error("Error loading data from %s: %s", file_path, error)
+        raise DataAggregationError from error
+
 def load_all_csv_files_from_directory(directory_path):
-    """
-    Load all CSV files from the specified directory and process them.
-    """
     dataframes = []
-    for file_name in os.listdir(directory_path):
-        if file_name.endswith(".csv"):
-            file_path = os.path.join(directory_path, file_name)
-            try:
-                loaded = load_csv(file_path)
-                time_column_name = loaded.columns[0]
-                aggregated = aggregate_to_day_based_prices(loaded,time_column_name)
-                converted = convert_datetime_to_unixtime(aggregated, time_column_name)
-                data_frame = add_symbol_by_file_name(converted, file_path)
-                dataframes.append(data_frame)
-                logger.info("Successfully loaded and added symbol for %s.", file_path)
-            except FileNotFoundError as exc:
-                logger.error("CSV file not found: %s", file_path)
-                raise CSVFileNotFoundError from exc
-            except Exception as error:
-                logger.error("Error loading data from %s: %s", file_path, error)
-                raise DataAggregationError from error
+    csv_files = [f for f in os.listdir(directory_path) if f.endswith('.csv')]
+
+    for file_name in csv_files:
+        file_path = os.path.join(directory_path, file_name)
+        try:
+            file = load_csv(file_path)
+            data_frame = process_single_csv_file(file, file_path)
+            dataframes.append(data_frame)
+        except (CSVFileNotFoundError, DataAggregationError):
+            continue
 
     return dataframes
