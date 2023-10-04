157bc40a52a7b857af68b40a8c95d6351953d8dc
PatrikTison <patriktison@gmail.com>
2023-10-04 15:39:47 +0200

    config was updated by pydantic

--- backend/postgres-seed/src/config.py
+++ backend/postgres-seed/src/config.py
@@ -5,34 +5,25 @@ This module uses Pydantic's BaseConfig for parsing and validation of configurati
 
 import os
 
-from dotenv import load_dotenv
 from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
 # Load environment variables from a .env file.
-load_dotenv()
 
-
-class GlobalConfig(BaseSettings):
+class Settings(BaseSettings):
     """
     Class for application-wide configuration settings.
     Reads environment variables and provides default values.
     """
+    title: str = os.environ.get("TITLE", "Default Title")
+    version: str = "1.0.0"
+    description: str = os.environ.get("DESCRIPTION", "Default Description")
+    openapi_prefix: str = os.environ.get("OPENAPI_PREFIX", "/")
+    docs_url: str = "/docs"
     openapi_url: str = "/openapi.json"
     api_prefix: str = "/api"
 
-    postgres_user: str = os.getenv("DB_USER")
-    postgres_password: str = os.getenv("DB_PASSWORD")
-    postgres_server: str = os.getenv("POSTGRES_SERVER")
-    postgres_port: int = int(os.getenv("DB_PORT"))
-    postgres_db: str = os.getenv("DB_NAME")
-
-    postgres_db_tests: str = os.environ.get("POSTGRES_DB_TESTS", "test_grayfox_db")
-
-    @property
-    def database_url(self) -> str:
-        """Construct and return the PostgreSQL database URL."""
-        return f"postgresql://{self.postgres_user}:{self.postgres_password}@{self.postgres_server}:{self.postgres_port}/{self.postgres_db}"
-
+    database_url: PostgresDsn = os.getenv("DB_URL")
+    postgres_db_tests: PostgresDsn = os.getenv("TEST_DB_URL")
 
 # Create an instance of the GlobalConfig class.
-settings = GlobalConfig()
+settings = Settings()
