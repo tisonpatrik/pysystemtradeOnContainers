5e2ab57d3c5a78b4813e68df0d5aabf4028c42d9
PatrikTison <patriktison@gmail.com>
2023-10-05 15:38:09 +0200

    docker updates... its broken

--- backend/postgres-seed/db/create.sql
+++ backend/postgres-seed/db/create.sql
@@ -1,11 +1,9 @@
 DROP DATABASE IF EXISTS grayfox_db;
 CREATE DATABASE grayfox_db;
 \connect grayfox_db;
-CREATE SCHEMA shakespeare;
-CREATE SCHEMA happy_hog;
+
 
 DROP DATABASE IF EXISTS test_grayfox_db;
 CREATE DATABASE test_grayfox_db;
 \connect test_grayfox_db;
-CREATE SCHEMA shakespeare;
-CREATE SCHEMA happy_hog;
+
