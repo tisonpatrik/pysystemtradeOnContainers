9178bdba23a95e45761979a01e292326f7ddef35
PatrikTison <patriktison@gmail.com>
2023-10-06 10:33:48 +0200

    create sql was updated

--- backend/postgres-seed/db/create.sql
+++ backend/postgres-seed/db/create.sql
@@ -1,10 +1,11 @@
-DROP DATABASE IF EXISTS grayfox_db;
-CREATE DATABASE grayfox_db;
-\connect grayfox_db;
-
 DROP DATABASE IF EXISTS test_grayfox_db;
 CREATE DATABASE test_grayfox_db;
 \connect test_grayfox_db;
 
-GRANT ALL PRIVILEGES ON DATABASE grayfox_db to "postgres";
 GRANT ALL PRIVILEGES ON DATABASE test_grayfox_db to "postgres";
+
+DROP DATABASE IF EXISTS grayfox_db;
+CREATE DATABASE grayfox_db;
+\connect grayfox_db;
+
+GRANT ALL PRIVILEGES ON DATABASE grayfox_db to "postgres";
