b3385416e0f56e97077ae7cbc1ba95b26c27245f
PatrikTison <patriktison@gmail.com>
2023-10-17 15:34:41 +0200

    new handler method was added

--- backend/postgres-seed/src/handlers/seed_db_handler.py
+++ backend/postgres-seed/src/handlers/seed_db_handler.py
@@ -2,24 +2,18 @@
 This module contains the SeedDBHandler class, 
 which is responsible for seeding the database from CSV files.
 """
-
+import asyncio
+import os
 import logging
 
+from src.configs.raw_data import settings
 from src.handlers.errors import DatabaseError
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import text
 
 # Initialize logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-import asyncio
-
-from src.data_processing.csv_helper import load_csv
-from src.db.repositories.data_inserter import DataInserter
-from src.db.schemas.base_config_schema import BaseConfigSchema
-from src.db.schemas.schemas import get_data_schemas
 
 
 class SeedDBHandler:
@@ -35,42 +29,23 @@ class SeedDBHandler:
         Asynchronously seed the database from CSV files using predefined schemas.
         """
 
-    async def get_available_seed_files_names_async(self):
+    async def get_count_of_mounted_files_async(self):
         """
-        Asynchronously seed the database from CSV files using predefined schemas.
+        Asynchronously count the number of CSV files mounted in the specified paths in the settings.
         """
+        mapping = settings.file_to_table_mapping
+        counts = {}
 
-    #     tasks = [
-    #         self._load_csv_and_insert_data_to_db_async(schema)
-    #         for schema in self.schemas
-    #     ]
-    #     results = await asyncio.gather(*tasks, return_exceptions=True)
+        async def count_csv_files_in_directory(directory):
+            return sum(1 for f in os.listdir(directory) if f.endswith(".csv"))
 
-    #     for result in results:
-    #         if isinstance(result, Exception):
-    #             logger.error("Error occurred while inserting data from CSV: %s", result)
+        tasks = []
+        for key, path in mapping.items():
+            tasks.append(asyncio.ensure_future(count_csv_files_in_directory(path)))
 
-    # async def _load_csv_and_insert_data_to_db_async(self, schema: BaseConfigSchema):
-    #     """
-    #     Asynchronously load data from a CSV file and insert it into the database as specified by the schema.
-    #     """
-    #     data_seeder = DataInserter(self.database_url)
-    #     try:
-    #         # Using the refactored load_csv_file_and_filename method
-    #         loaded_data = load_csv(schema.file_path)
+        completed, _ = await asyncio.gather(*tasks)
 
-    #         # Extract DataFrame and file name from the returned dictionary
-    #         data_frame = loaded_data["dataframe"]
-    #         file_name = loaded_data["file_name"]
+        for idx, (key, _) in enumerate(mapping.items()):
+            counts[key] = completed[idx]
 
-    #         # Log the action
-    #         logger.info(f"Processing CSV file: {file_name}")
-
-    #         # Insert the DataFrame into the database
-    #         await data_seeder.insert_dataframe_async(data_frame, schema.table_name)
-
-    #     except Exception as error:
-    #         logger.error(
-    #             f"Error occurred while processing the CSV file {schema.file_path}: {error}"
-    #         )
-    #         raise error
+        return counts
