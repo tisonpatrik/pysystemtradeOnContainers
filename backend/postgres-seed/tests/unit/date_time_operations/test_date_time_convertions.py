3a1a6012d43c86e015b58d5ee87d8e7f1d1c3793
PatrikTison <patriktison@gmail.com>
2023-10-28 17:34:04 +0200

    end of test... pylint incoming

--- backend/postgres-seed/tests/unit/date_time_operations/test_date_time_convertions.py
+++ backend/postgres-seed/tests/unit/date_time_operations/test_date_time_convertions.py
@@ -1,11 +1,17 @@
+import time
 import pytest
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
 from src.common_utils.utils.date_time_operations.date_time_convertions import (
     convert_column_to_datetime,
+    convert_column,
+    convert_datetime_to_unixtime,
 )
-from src.common_utils.errors.date_time_errors import InvalidDatetimeColumnError
+from src.common_utils.errors.date_time_errors import (
+    InvalidDatetimeColumnError,
+    DateTimeConversionError,
+)
 
 
 @pytest.fixture
@@ -39,3 +45,30 @@ def test_convert_unix_to_datetime(time_based_dataframe):
 def test_convert_invalid_column(time_based_dataframe):
     with pytest.raises(InvalidDatetimeColumnError):
         convert_column_to_datetime(time_based_dataframe, "invalid_column")
+
+
+# Test for convert_column
+def test_convert_column(time_based_dataframe):
+    df = time_based_dataframe.copy()
+    df["time_str"] = pd.to_datetime(df["time_str"])
+    result = convert_column(time_based_dataframe, "time_str", None)
+    assert_frame_equal(result, df)
+
+
+# Test for convert_datetime_to_unixtime
+def test_convert_datetime_to_unixtime(time_based_dataframe):
+    df = time_based_dataframe.copy()
+    df["time_str"] = pd.to_datetime(df["time_str"])
+    df["time_unix_converted"] = df["time_str"].apply(
+        lambda x: int(time.mktime(x.timetuple()))
+    )
+    result = convert_datetime_to_unixtime(df, "time_str")
+    assert_frame_equal(result, df)
+
+
+# Test for exception in convert_datetime_to_unixtime
+def test_convert_datetime_to_unixtime_exception(time_based_dataframe):
+    df = time_based_dataframe.copy()
+    df["time_str"] = ["not_a_datetime", "2021-01-02"]
+    with pytest.raises(DateTimeConversionError):
+        convert_datetime_to_unixtime(df, "time_str")
