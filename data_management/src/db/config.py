5bcda54ccf4968c11ca4c7f4aeb65057ccdff5a2
PatrikTison <patriktison@gmail.com>
2023-11-27 15:32:51 +0100

    get rid of with .env

--- data_management/src/db/config.py
+++ data_management/src/db/config.py
@@ -3,26 +3,17 @@ Module for configuring global settings for the application.
 This module uses Pydantic's BaseConfig for parsing and validation of configuration data.
 """
 
-import os
-
 from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
-
-from dotenv import load_dotenv
-
-# Load environment variables from a .env file.
-load_dotenv()
-
 
 class Settings(BaseSettings):
     """
     Class for application-wide configuration settings.
-    Reads environment variables and provides default values.
+    Directly provides configuration values.
     """
 
-    database_url: PostgresDsn = os.getenv("DB_URL")  # type: ignore
-    test_database_url: PostgresDsn = os.getenv("TEST_DB_URL")  # type: ignore
+    database_url: PostgresDsn = "postgresql+asyncpg://postgres:postgres@postgres/grayfox_db"
+    test_database_url: PostgresDsn = "postgresql+asyncpg://postgres:postgres@postgres/test_db"
 
-
-# Create an instance of the GlobalConfig class.
+# Create an instance of the Settings class.
 settings = Settings()
