2fb6416d968434bbffd4e4cd527b99a80c963e7f
tisonpatrik <patriktison@gmail.com>
2024-02-13 19:34:33 +0100

    Remove unused test file

--- data_management/tests/unit/risk/daily_returns_volatility_test.py
+++ data_management/tests/unit/risk/daily_returns_volatility_test.py
@@ -9,14 +9,32 @@ def load_csv_data(filename):
     return pd.Series(data["price"])
 
 
-def test_process_daily_returns():
+def test_daily_returns():
     # Load input and expected data
     daily_prices = load_csv_data("daily_prices")
     expected_vol = load_csv_data("expected_daily_returns")
 
-    # Initialize the estimator and calculate volatility
     estimator = DailyReturnsVolEstimator()
     calculated_vol = estimator.daily_returns(daily_prices)
+
+    pd.testing.assert_series_equal(
+        calculated_vol,
+        expected_vol,
+        check_dtype=True,
+        rtol=1e-5,
+        atol=1e-5,
+        check_index_type="equiv",
+        obj="CalculatedVol",
+    )
+
+
+def test_process_daily_returns_vol():
+    # Load input and expected data
+    daily_prices = load_csv_data("daily_prices")
+    expected_vol = load_csv_data("expected_daily_returns_vol")
+
+    estimator = DailyReturnsVolEstimator()
+    calculated_vol = estimator.process_daily_returns_vol(daily_prices)
 
     pd.testing.assert_series_equal(
         calculated_vol,
