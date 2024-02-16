9e3150c9bf1f4098fab3a7a853a8f3b2e50e88a2
tisonpatrik <patriktison@gmail.com>
2024-02-16 11:24:16 +0100

    instrument_volatility_test added but not worked

---
+++ data_management/tests/unit/risk/instrument_volatility_test.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from src.risk.estimators.instrument_volatility import InstrumentVolEstimator
+
+
+def load_csv_data(filename):
+    filepath = f"tests/test_data/{filename}.csv"
+    data = pd.read_csv(filepath)
+    return pd.Series(data["price"])
+
+
+def test_instrument_volatility():
+    # Load input and expected data
+    multiple_prices = load_csv_data("multiple_prices")
+    daily_returns_vol = load_csv_data("expected_daily_returns")
+    exptected = load_csv_data("expected_instrument_vol")
+    estimator = InstrumentVolEstimator()
+    calculated_vol = estimator.get_instrument_currency_vol(
+        multiple_prices, daily_returns_vol, point_size=200
+    )
+    pd.testing.assert_series_equal(
+        calculated_vol,
+        exptected,
+        check_dtype=True,
+        rtol=1e-5,
+        atol=1e-5,
+        check_index_type="equiv",
+        obj="CalculatedVol",
+    )
