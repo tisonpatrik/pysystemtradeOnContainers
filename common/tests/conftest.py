00954a22f066e986debaaaf7c2631f2da04c391e
tisonpatrik <patriktison@gmail.com>
2024-03-04 12:02:37 +0100

    Add sys.path modifications to pytest_configure()***

--- common/tests/conftest.py
+++ common/tests/conftest.py
@@ -1,5 +1,19 @@
-# conftest.py is a file that is used to define fixtures for your tests. Fixtures are a way to provide data, test doubles, or state setup to your tests.
-import os
+import sys
+from pathlib import Path
 
-print("VOOOOOOOOOLEEEEEEEEEEEEE")
-print(os.environ.get("PYTHONPATH"))
+
+def pytest_configure():
+    # Base directory for the project
+    root_dir = Path(__file__).parent.parent
+
+    # Paths to add to sys.path, pointing to the base of `data_management` and `common`
+    paths_to_add = [root_dir]
+
+    # Convert paths to strings and add them to sys.path if not already included
+    for path in paths_to_add:
+        str_path = str(path)
+        if str_path not in sys.path:
+            sys.path.insert(0, str_path)
+
+    # Print sys.path for debugging
+    print("Modified sys.path to include:", *paths_to_add, sep="\n")
