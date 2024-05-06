639d2331be549d2d214cf8f7306487ca3135495a
tisonpatrik <patriktison@gmail.com>
2024-05-06 19:22:44 +0200

    Refactor positions handler to compute average position using volatility scalar

--- positions/src/api/handlers/positions_handler.py
+++ positions/src/api/handlers/positions_handler.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from fastapi import Depends, HTTPException
-from httpx import get
 
 from common.src.database.repository import Repository
 from common.src.dependencies.core_dependencies import get_client, get_repository
@@ -9,7 +8,6 @@ from common.src.logging.logger import AppLogger
 from common.src.models.api_query_models import GetFxRateQuery, GetInstrumentCurrencyVolQuery
 from positions.src.api.models.positions_request_model import SubsystemPositionForInstrument
 from positions.src.services.cash_volatility_target_service import CashVolTargetService
-from positions.src.services.volatility_scalar_service import VolatilityScalarService
 
 
 class PositionsHandlers:
@@ -20,42 +18,40 @@ class PositionsHandlers:
     ):
         self.logger = AppLogger.get_instance().get_logger()
         self.cash_vol_target_service = CashVolTargetService()
-        self.volatility_scalar_service = VolatilityScalarService()
         self.repository = repository
         self.client = client
 
     async def get_subsystem_position_async(self, request: SubsystemPositionForInstrument) -> pd.Series:
         try:
             self.logger.info("Starting to get average position at subsystem level.")
-            avarage_forecast = request.avarage_absolute_forecas
+            avg_abs_forecast = request.avarage_absolute_forecas
 
-            # vol scalar
-
-            instr_value_vol = await self.get_instrument_value_vol(request.instrument_code, request.base_currency)
+            fx_rate = await self.get_fx_rates_async(request.instrument_code, request.base_currency)
+            instr_ccy_vol = await self.get_instrument_volatility_async(request.instrument_code)
+            indexed = fx_rate.reindex(instr_ccy_vol.index, method="ffill")
+            instr_value_vol = instr_ccy_vol.ffill() * indexed
             cash_vol_target = self.cash_vol_target_service.get_daily_cash_vol_target(
                 request.notional_trading_capital, request.percentage_volatility_target
             )
-            vol_scalar = self.volatility_scalar_service.get_volatility_scalar(cash_vol_target, instr_value_vol)
+            vol_scalar = cash_vol_target / instr_value_vol
 
-            # forecast
-
+            # forecast = self.get_combined_forecast(request.instrument_code)
+            # vol_scalar_reindexed = vol_scalar.reindex(forecast.index, method="ffill")
+            # subsystem_position_raw = vol_scalar_reindexed * forecast / avg_abs_forecast
+            # subsystem_position = self._apply_long_only_constraint_to_position(
+            #     positions=subsystem_position_raw, instrument_code=request.instrument_code
+            # )
             self.logger.info("Successfully computed average position.")
             return vol_scalar
         except Exception as e:
             self.logger.error("Failed to compute average position: %s", str(e))
             raise HTTPException(status_code=500, detail=str(e))
 
-    async def get_instrument_value_vol(self, instrument_code: str, base_currency: str) -> pd.Series:
-        try:
-            fx_rate = await self.get_fx_rates_async(instrument_code, base_currency)
-            instr_ccy_vol = await self.get_instrument_volatility_async(instrument_code)
-            indexed = fx_rate.reindex(instr_ccy_vol.index, method="ffill")
-            instr_value_vol = instr_ccy_vol.ffill() * indexed
-            self.logger.info("Successfully computed instrument value volatility.")
-            return instr_value_vol
-        except Exception as e:
-            self.logger.error(f"Error computing value volatility for {instrument_code}: {str(e)}")
-            raise HTTPException(status_code=500, detail="Error in processing instrument value volatility")
+    def get_combined_forecast(self, instrument_code: str):
+        return pd.Series(1)
+
+    def _apply_long_only_constraint_to_position(self, positions: pd.Series, instrument_code: str) -> pd.Series:
+        return positions
 
     async def get_fx_rates_async(self, instrument_code: str, base_currency: str) -> pd.Series:
         try:
