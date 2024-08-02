c59c58869df625792c81c3f786c90e273299c05e
tisonpatrik <patriktison@gmail.com>
2024-08-02 12:56:17 +0200

    it seems that breakout and accel works

--- rules/src/api/handlers/accel_handler.py
+++ rules/src/api/handlers/accel_handler.py
@@ -4,8 +4,6 @@ from common.src.cqrs.api_queries.get_rule_for_instrument import GetRuleForInstru
 from common.src.logging.logger import AppLogger
 from common.src.repositories.prices_repository import PricesRepository
 from common.src.repositories.risk_client import RiskClient
-from common.src.utils.convertors import series_to_dataframe
-from common.src.validation.trading_signal import TradingSignal
 from rules.src.services.accel import AccelService
 
 
@@ -16,13 +14,14 @@ class AccelHandler:
         self.prices_repository = prices_repository
         self.risk_client = risk_client
 
-    async def get_accel_async(self, request: GetRuleForInstrumentQuery) -> pd.DataFrame:
+    async def get_accel_async(self, request: GetRuleForInstrumentQuery) -> pd.Series:
         try:
             self.logger.info(f"Calculating Accel rule for {request}")
             daily_prices = await self.prices_repository.get_daily_prices_async(request.symbol)
             vol = await self.risk_client.get_daily_retuns_vol_async(request.symbol)
             accel = self.accel_service.calculate_accel(daily_prices, vol, request.speed)
-            return series_to_dataframe(accel, TradingSignal, str(TradingSignal.date_time), str(TradingSignal.value))
+            accel = accel.dropna()
+            return accel
         except Exception as e:
             self.logger.error(f"Error calculating accel rule for {request}: {str(e)}")
             raise e
--- rules/src/api/handlers/breakout_handler.py
+++ rules/src/api/handlers/breakout_handler.py
@@ -1,9 +1,9 @@
+import pandas as pd
+
 from common.src.cqrs.api_queries.get_rule_for_instrument import GetRuleForInstrumentQuery
 from common.src.http_client.rest_client import RestClient
 from common.src.logging.logger import AppLogger
 from common.src.repositories.prices_repository import PricesRepository
-from common.src.utils.convertors import series_to_dataframe
-from common.src.validation.trading_signal import TradingSignal
 from rules.src.services.breakout import BreakoutService
 
 
@@ -14,12 +14,13 @@ class BreakoutHandler:
         self.prices_repository = prices_repository
         self.client = client
 
-    async def get_breakout_async(self, request: GetRuleForInstrumentQuery):
+    async def get_breakout_async(self, request: GetRuleForInstrumentQuery) -> pd.Series:
         try:
             self.logger.info(f"Calculating Breakout rule for {request}")
             daily_prices = await self.prices_repository.get_daily_prices_async(request.symbol)
             breakout = self.breakout_service.calculate_breakout(daily_prices, request.speed)
-            return series_to_dataframe(breakout, TradingSignal, str(TradingSignal.date_time), str(TradingSignal.value))
+            breakout = breakout.dropna()
+            return breakout
         except Exception as e:
             self.logger.error(f"Error calculating breakout rule for {request}: {str(e)}")
             raise e
