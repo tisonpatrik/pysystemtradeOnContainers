d06e1ec64fd7e3ba5383ed24ed0a468c9d6160a3
tisonpatrik <patriktison@gmail.com>
2024-09-15 14:15:39 +0200

    first migrations was added

---
+++ database/migrations/000001_create_table.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS spread_costs;
+DROP TABLE IF EXISTS roll_config;
+DROP TABLE IF EXISTS instrument_metadata;
+DROP TABLE IF EXISTS instrument_config;
---
+++ database/migrations/000001_create_table.up.sql
@@ -0,0 +1,35 @@
+CREATE TABLE instrument_config (
+    symbol VARCHAR PRIMARY KEY,
+    description VARCHAR,
+    pointsize FLOAT,
+    currency VARCHAR,
+    asset_class VARCHAR,
+    per_block FLOAT,
+    percentage FLOAT,
+    per_trade INTEGER,
+    region VARCHAR
+);
+
+CREATE TABLE instrument_metadata (
+    symbol VARCHAR PRIMARY KEY,
+    asset_class VARCHAR,
+    sub_class VARCHAR,
+    description VARCHAR,
+    CONSTRAINT fk_symbol FOREIGN KEY(symbol) REFERENCES instrument_config(symbol)
+);
+
+CREATE TABLE roll_config (
+    symbol VARCHAR PRIMARY KEY,
+    hold_roll_cycle VARCHAR,
+    roll_offset_days INTEGER,
+    carry_offset INTEGER,
+    priced_roll_cycle VARCHAR,
+    expiry_offset INTEGER,
+    CONSTRAINT fk_symbol_roll FOREIGN KEY(symbol) REFERENCES instrument_config(symbol)
+);
+
+CREATE TABLE spread_costs (
+    symbol VARCHAR PRIMARY KEY,
+    spread_costs FLOAT,
+    CONSTRAINT fk_symbol_spread FOREIGN KEY(symbol) REFERENCES instrument_config(symbol)
+);
